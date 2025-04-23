d2eee629aa3093882f1719e145abcc28d02c2e92
MagnusE22R <143402790+magnusqe@users.noreply.github.com>
2025-04-23 16:35:35 +0200

    Function manager v.2
    change function feature

--- function_manager.py
+++ function_manager.py
@@ -8,14 +8,9 @@ class Function_Manager:
     def make_new_function(self, input):
         func = fs.Function(input)
         self.function_list.append(func)
-
-    def search_function(self):
-        # To be made
-        d = 1
         
-    def change_function(self, func):
-        # To be made
-        d = 1
+    def change_function(self, func, index):
+        self.function_list[index] = self.make_new_function(func)
         
     def get_functions(self):
         return self.function_list
