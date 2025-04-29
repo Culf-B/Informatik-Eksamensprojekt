df6f37532ff1bd431c48a7732439a1d49e08590a
MagnusE22R <143402790+magnusqe@users.noreply.github.com>
2025-04-29 13:58:33 +0200

    Function manager v.3
    input determines if function is getting changed or new function is made

--- function_manager.py
+++ function_manager.py
@@ -1,16 +1,33 @@
 import function_script as fs
 
-
 class Function_Manager:
     def __init__(self):
         self.function_list = []
+
+    def choose_action(self, input):
+        input = input.replace(" ", "")
+        func_name = None
+
+        if "=" in input and len(input.split("=")[0]) > 2:
+            lhs = input.split("=")[0]
+            func_name = lhs[0]  # 'f' in "f(x)"
+
+        if func_name:
+            for i, func in enumerate(self.function_list):
+                if func.func_name == func_name:
+                    self.change_function(input, i)
+        else:
+            self.make_new_function(input)
         
     def make_new_function(self, input):
         func = fs.Function(input, self)
         self.function_list.append(func)
         
-    def change_function(self, func, index):
-        self.function_list[index] = self.make_new_function(func)
+    def change_function(self, input, index):
+        existing_name = self.function_list[index].func_name
+        func = fs.Function(input, self, existing_name)
+        self.function_list[index] = func
+
         
     def get_functions(self):
         return self.function_list
