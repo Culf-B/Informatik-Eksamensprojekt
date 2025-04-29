7f9f07577e22f903c038ef8adafc478d62b42645
MagnusE22R <143402790+magnusqe@users.noreply.github.com>
2025-04-29 14:48:07 +0200

    Function manager v.3.2
    Bug fix

--- function_manager.py
+++ function_manager.py
@@ -11,13 +11,23 @@ class Function_Manager:
         if "=" in input and len(input.split("=")[0]) > 2:
             lhs = input.split("=")[0]
             func_name = lhs[0]  # 'f' in "f(x)"
+        else:
+            self.make_new_function
 
-        if func_name and func_name in self.function_list:
+        print(func_name)
+
+        if func_name and len(self.function_list) > 0:
+            print("that")
             for i, func in enumerate(self.function_list):
                 if func.func_name == func_name:
+                    print("here")
                     self.change_function(input, i)
+                else:
+                    print("this")
+                    self.make_new_function(input)
         else:
             self.make_new_function(input)
+            print("sigma")
         
     def make_new_function(self, input):
         func = fs.Function(input, self)
@@ -35,4 +45,5 @@ if __name__ == '__main__':
     fm = Function_Manager()
     fm.choose_action("f(x) = 5x")
     fm.choose_action("f(x) = 3*x^2")
-    print((fm.get_functions()[0].func), (fm.get_functions()[1].func))
+    print(fm.get_functions()[0].func)
+    
